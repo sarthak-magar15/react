f1edea43c524648d9de722788b1c92ace9f50469
sarthak-magar15 <magarsarthak15@gmail.com>
2023-05-30 12:37:36 +0530

    Dark modes changes to first react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from'./components/Navbar';
+//import TextForm from './components/TextForm';
+import About from './components/About';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false); // whether dark mode is enabled or not 
   return (
-    <>
-    {/*
-    <Navbar title="TextUtils2" aboutText = "About TextUtils"/>
-  */}
-    <Navbar title= "TextUtils"/>
+   <>
+    {/*<TextForm heading ="enter the etxt to analyse"/>  */ }
+    <Navbar title="TextUtils" mode={darkMode} />
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze"/>
+    <TextForm heading= "Enter the text to analyze below "/>
     </div>
-    
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,6 +5,18 @@ export default function TextForm(props) {
     console.log("Uppercase was clicked" + text); 
     let newText = text.toUpperCase();
     setText(newText)
+  }  
+
+  const handleLowClick = () =>{
+    console.log("Lowercase was clicked" + text); 
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+
+  const handleClick = () =>{
+    console.log("Copy to was clicked" + text); 
+    let newText = text.toLowerCase();
+    setText(newText)
   }
   const handleOnChange = (event) =>{
     console.log("On change"); 
@@ -15,12 +27,24 @@ export default function TextForm(props) {
   //setText = ("New text here "); // this is the correct way 
 
   return (
-    <div>
+    <>
+    <div className = "container">
       <h1>{props.heading}</h1>
         <div className="form-group">
             <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase </button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To UpperCase </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert To LowerCase </button>
+        <button className="btn btn-primary mx-2" onClick={handleClick}>Copy To Clipboard  </button>
     </div>
+    <div classname = "container my-2">
+      <h1>Your text summary </h1>
+      <p>{text.split(" ").length} words and {text.length} Characters </p>
+      <p>{0.008*text.split(" ").length} minutes to read </p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
+    
   )
 }
